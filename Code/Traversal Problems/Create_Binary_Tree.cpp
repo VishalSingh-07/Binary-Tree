0a3c9756ea25afda4c6ef66e0d7a66a4fdc577c0
VishalSingh-07 <vishalsinghagr25@gmail.com>
2024-02-26 22:08:37 +0530

    Traversal Problem

---
+++ Code/Traversal Problems/Create_Binary_Tree.cpp
@@ -0,0 +1,139 @@
+/*
+Create Binary Tree
+
+Problem statement
+Given an array 'arr' that contains 7 integers representing the values of nodes in a binary tree. This represents level order. The first element of the array represents the value of the root node.
+
+
+Your objective is to construct a binary tree using the remaining 6 elements of the array, creating nodes for each of these values and return root node.
+
+
+
+For example:
+arr = [11, 22, 3, 54, 15, 23, 21]
+The 7 node binary tree is represented below.
+
+Detailed explanation ( Input/output format, Notes, Images )
+
+
+Sample Input 1:
+1 2 3 4 5 6 7
+Sample Output 1:
+1
+Explanation Of Sample Input 1:
+The 7 node binary tree is represented below.
+
+Sample Input 2:
+11 22 3 54 15 23 21
+Sample Output 2:
+11
+
+
+Constraints:
+arr.length = 7
+1<=arr[i]<=100
+Time Limit: 1 sec
+*/
+
+
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node()
+    {
+        this->data = 0;
+        left = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->left = NULL;
+        this->right = NULL;
+    }
+    Node(int data, Node* left, Node* right)
+    {
+        this->data = data;
+        this->left = left;
+        this->right = right;
+    }
+};
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node()
+    {
+        this->data = 0;
+        left = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->left = NULL;
+        this->right = NULL;
+    }
+    Node(int data, Node* left, Node* right)
+    {
+        this->data = data;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+// Optimized Approach
+// Time complexity -> O(n) and Space -> O(n)
+
+Node* createTree(vector<int>&arr){
+    // Write your code here
+    Node *p,*t,*root;
+    queue<Node*> q;
+    root=new Node;
+    root->data=arr[0];
+    root->left=root->right=nullptr;
+    q.push(root);
+
+    int i=1,n=arr.size();
+    while(i<n)
+    {
+        p=q.front();
+        q.pop();
+        if(arr[i]!=-1)
+        {
+            t=new Node;
+            t->data=arr[i];
+            t->left=t->right=nullptr;
+            p->left=t;
+            q.push(t);
+        }
+        i++;
+        if(i < n && arr[i]!=-1)
+        {
+            t=new Node;
+            t->data=arr[i];
+            t->left=t->right=nullptr;
+            p->right=t;
+            q.push(t);
+        }
+        i++;
+    }
+    return root;
+}
+
+/*
+1. Question link -- https://www.codingninjas.com/studio/problems/create-binary-tree_8360671
+
+2. Video Link -- https://youtu.be/ctCpP0RFDFc
+*/
